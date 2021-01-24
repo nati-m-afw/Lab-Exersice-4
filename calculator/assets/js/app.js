97ebad394e2210006920144e9df612ad218b19cd
nati-m <it.natnael.mekonnen@gmail.com>
2021-01-24 19:52:01 +0300

    added menu for calculator

--- calculator/assets/js/app.js
+++ calculator/assets/js/app.js
@@ -1,3 +1,73 @@
+var operator;
+var input = [];
+
+
+// POP UP MENU
+(function(){
+    var operator = prompt("Select an operation: \n \t1. Add\n \t2. Subtract\n \t3. Multiply\n \t4. Divide\n \t5. Square\n \t6. Square Root");
+    
+// TAKING INPUT BASED ON CHOICE OF OPERATOR   
+    switch (operator) {
+        case "1":
+        case "3":
+            input.push(     parseFloat(prompt("Enter number:"))     );
+            do{
+                input.push(     parseFloat(prompt("Enter number:"))     );
+            }while (    prompt("Do you want to enter more numbers? ( Y / N )").toLowerCase() == "y"   )
+        
+            break;
+    
+        case "2":
+        case "4":
+            input.push(     parseFloat(prompt("Enter number:"))     );
+            input.push(     parseFloat(prompt("Enter number:"))     );
+            break;
+        
+        case "5":
+        case "6":
+            input.push(     parseFloat(prompt("Enter number:"))     );
+            break;
+             
+        default:
+            console.log("Wrong input!");
+            break;
+    }
+    
+// CALCULATING BASED ON CHOICE OF OPERATION
+    switch (operator) {
+        case "1":
+            console.log(add(input));
+            break;
+    
+        case "2":
+            console.log(subtract(input));
+            break;
+        
+        case "3":
+            console.log(multiply(input));
+            break;
+        
+        case "4":
+            console.log(divide(input));
+            break;
+        
+        case "5":
+            console.log(square(input));
+            break;
+    
+        case "6":
+            console.log(root(input));
+            break;
+            
+        
+        default:
+            console.log("Wrong input!");
+            break;
+    }
+})();
+
+
+// MATH FUNCTIONS
 function add(numbers){
     var result = 0;
     numbers.forEach(function(number) {
@@ -6,8 +76,8 @@ function add(numbers){
     return result;
 }
 
-function subtract(x, y){
-    return x - y;
+function subtract(numbers){
+    return numbers[0] - numbers[1];
 }
 
 function multiply(numbers){
@@ -18,14 +88,14 @@ function multiply(numbers){
     return result;
 }
 
-function divide(x, y){
-    return y != 0 ? x / y : "Wrong input!";
+function divide(numbers){
+    return numbers[1] != 0 ? numbers[0] / numbers[1] : "Wrong input!";
 }
 
-function square(x){
-    return x * x;
+function square(numbers){
+    return numbers[0] * numbers[0];
 }
 
-function root(x){
-    return x >= 0 ? Math.sqrt(x) : "Wrong input!";
+function root(numbers){
+    return numbers[0] >= 0 ? Math.sqrt(numbers[0]) : "Wrong input!";
 }
